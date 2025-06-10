a4d8a94916dc127c714dfc8084ba57aeea9d5709
escha-k <bbs7380@gmail.com>
2025-06-10 16:50:37 +0900

    Project init, check TODO

---
+++ src/main/java/com/zerobase/fastlms/admin/controller/AdminBannerController.java
@@ -0,0 +1,7 @@
+package com.zerobase.fastlms.admin.controller;
+
+/**
+ * TODO : 배너 관리 페이지 만들기
+ */
+public class AdminBannerController {
+}
---
+++ src/main/java/com/zerobase/fastlms/member/entity/LoginHistory.java
@@ -0,0 +1,7 @@
+package com.zerobase.fastlms.member.entity;
+
+/**
+ * TODO : 로그인 히스토리 엔티티 구현
+ */
+public class LoginHistory {
+}
